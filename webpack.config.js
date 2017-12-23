ece23a81020121f267e709d16195a5a557b6b741
xudafeng <xudafeng@126.com>
2017-12-23 23:44:01 +0800

    update babel

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,7 @@ module.exports = {
         loader: 'babel-loader',
         exclude: /node_modules/,
         query: {
-          presets: ['es2015', 'stage-2']
+          presets: ['latest', 'stage-2']
         }
       },
       {
